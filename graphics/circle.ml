11d8dcb087c8c17779f550c819e7c792e6d77155
Bramford Horton <bram.horton@gmail.com>
2017-02-05 23:27:09 +1300

    Continue learning - https://ocaml.org/learn/tutorials/structure_of_ocaml_programs.html

--- graphics/circle.ml
+++ graphics/circle.ml
@@ -1,9 +1,25 @@
 open Graphics;;
 
+let pick_color x = match x with
+  | ((x mod 5), 0) -> green
+  | ((x mod 4), 0) -> purple
+  | (x mod 3, 0) -> blue
+  | (x mod 2, 0) -> yellow
+  | (x mod 1, 0) -> red
+  ;;
+
+
+
+
 open_graph " 640x480";;
-for i = 12 downto 1 do
+for i = Random.int (5 * 4 * 3 * 2) downto 1 do
   let radius = i * 20 in
-  set_color (if i mod 2 = 0 then red else yellow);
+  let set_color = match i with
+      | i mod 5 = 0 -> green
+      | i mod 4 = 0 -> purple
+      | i mod 3 = 0 -> blue
+      | i mod 2 = 0 -> yellow
+      | i mod 1 = 0 -> red
   fill_circle 320 240 radius
 done;;
 read_line ();;
