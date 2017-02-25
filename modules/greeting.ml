afaa8c0dce5808b4658592202a0b607843507c1c
Bramford Horton <bram.horton@gmail.com>
2017-02-25 21:16:35 +1300

    Add submodule interfaces

--- modules/greeting.ml
+++ modules/greeting.ml
@@ -1,5 +1,13 @@
-module Greeting = struct
-  module Hello = struct
+module type Greeting_type = sig
+  val goodbye : unit -> unit
+  val hello_goodbye : unit -> unit
+  module Hello : sig
+    val hello : unit ->  unit
+  end
+end
+
+module Greeting : Greeting_type = struct
+  module Hello  = struct
     let message = "Hello"
     let hello () = print_endline message
   end
@@ -7,4 +15,5 @@ module Greeting = struct
   let hello_goodbye () =
     Hello.hello ();
     goodbye ()
-end
+end;;
+
