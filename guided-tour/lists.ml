176761c6269d76537317af18375519a950e708c1
Bramford Horton <bram.horton@gmail.com>
2017-05-27 07:41:41 +1200

    Continue lists

---
+++ guided-tour/lists.ml
@@ -0,0 +1,17 @@
+let languages = ["C";"OCaml";"Python"]
+
+
+let elements_over x l =
+
+  List.sort Pervasives.compare l |> fun l_sorted ->
+  let rec loop i = function
+    match l with
+  
+
+  List.for_all (fun x -> x > 2) numbers |> fun original_above_two ->
+  if not original_above_two then
+  List.tl numbers |> fun numbers_short ->
+  List.for_all
+  all_above_two;;
+
+elements_over 3 [1;2;3;4;5]
