0afdef51094e56e29be3cb52bb938478d3cca63d
Bramford Horton <bram.horton@gmail.com>
2017-06-02 21:18:05 +1200

    Playing with lists

--- guided-tour/lists.ml
+++ guided-tour/lists.ml
@@ -1,5 +1,63 @@
 let languages = ["C";"OCaml";"Python"]
 
+let rec sum l =
+  match l with
+  | [] -> 0
+  | x :: y -> x + sum y 
+;;
+
+sum [1;2;3];;
+sum (1 :: 2 :: 3 :: []) ;;
+
+
+
+(* Iterate over list of integers. All integers that return true with [f] will be appended to a returned list. If no integers match the condition, return an empty list *)
+
+ 
+(* get_input "Enter b" |> fun b ->*)
+(*
+let () =
+  let new_list = [];;
+  List.iter (fun x -> x > 3) int_list |>
+*)
+  
+
+  
+(*
+let rec check_over y l =
+  match l with
+  | [] -> []
+  | hd :: tl -> 
+    if (hd > y) then check_over (y (hd :: tl)) else check_over (y tl)
+;;
+   *)
+
+let int_list = [1;2;3];;
+List.sort (Pervasives.compare int_list) |> fun int_list_s ->
+let int_list_over_three = List.filter (fun x -> x > 3) int_list_s ;;
+int_list_over_three;;
+
+(*
+let hd = [1]
+let tl = [2;3]
+let y = 3
+if (hd > y) then check_over (y (hd :: tl)) else check_over (y tl)
+
+let rec check_over y l =
+  match l with
+  | [] -> []
+  | [hd] -> 
+    if (hd >= y) then [hd] else []
+  | hd :: tl ->
+    if (hd >= y) then (check_over (y (tl :: hd)))
+    else (check_over (y [tl]))
+;;
+
+(*
+List.iter int_list (x > 3)
+
+let  = function
+  | 
 
 let elements_over x l =
 
@@ -15,3 +73,4 @@ let elements_over x l =
   all_above_two;;
 
 elements_over 3 [1;2;3;4;5]
+   *)
