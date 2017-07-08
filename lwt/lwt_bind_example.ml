7854411f2906934b9799a4389cf19f10e006af58
Bramford Horton <bram.horton@gmail.com>
2017-07-08 23:18:33 +1200

    Add lwt map example. Fix lwt bind example

--- lwt/lwt_bind_example.ml
+++ lwt/lwt_bind_example.ml
@@ -1,12 +1,12 @@
-#require "lwt";;
+(*#require "lwt";;*)
 open Lwt
 
 let get_input prompt =
-  print_string prompt;
+  print_string (prompt ^ ": ");
   Lwt.return (read_int ())
 
 let x =
   get_input "Enter a" >>= fun a ->
   get_input "Enter b" >>= fun b ->
-  Lwt.return (a + b)
-
+  Lwt.return (Printf.printf "%d + %d = %d" a b (a + b))
+;;
