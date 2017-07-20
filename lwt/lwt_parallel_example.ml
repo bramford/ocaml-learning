6aca52e6b1ed9c1cf1ef9e8e49f4a8e296bcecdf
Bramford Horton <bram.horton@gmail.com>
2017-07-20 22:04:36 +1200

    Fix output order with flush

--- lwt/lwt_parallel_example.ml
+++ lwt/lwt_parallel_example.ml
@@ -1,19 +1,20 @@
 open Lwt.Infix
 
 let wait_time n =
-  Lwt_io.printf "%f started\n" n  >>= fun () ->
+  Lwt_io.printf "%f started\n%!" n  >>= fun () ->
   Lwt_unix.sleep (4.0 -. n) >>= fun () ->
-  Lwt_io.printf "%f done\n" n
+  Lwt_io.printf "%f done\n%!" n >>= fun () ->
+  Lwt_io.flush Lwt_io.stdout
 
 let () =
   let start_time = Unix.gettimeofday () in
-  Printf.printf "Start time: %f\n" start_time;
+  Printf.printf "Start time: %f\n%!" start_time;
 
   Lwt_main.run (Lwt_list.iter_p (wait_time) [1.0;2.0;3.0]);
 
   let end_time = Unix.gettimeofday () in
-  Printf.printf "End time: %f\n" end_time;
-  Printf.printf "Took: %f\n" (end_time -. start_time);
+  Printf.printf "End time: %f\n%!" end_time;
+  Printf.printf "Took: %f\n%!" (end_time -. start_time);
 
 (*
   Output:
