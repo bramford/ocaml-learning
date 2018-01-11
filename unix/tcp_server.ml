c76952b8bdbe7f3b361caa3889a5975557db3b0a
Bramford Horton <bram.horton@gmail.com>
2018-01-12 00:11:16 +1300

    Add unix tcp server example

---
+++ unix/tcp_server.ml
@@ -0,0 +1,59 @@
+(* Example unix tcp server, originally derived from
+   https://caml.inria.fr/pub/docs/oreilly-book/html/book-ora187.html *)
+
+let establish_server server_fun sockaddr =
+  let domain = Unix.domain_of_sockaddr sockaddr in
+  let sock = Unix.socket domain Unix.SOCK_STREAM 0 in
+  Unix.bind sock sockaddr ;
+  Unix.listen sock 3 ;
+    while true do
+      let (s, caller) = Unix.accept sock 
+      in match Unix.fork() with
+        | 0 -> if Unix.fork() <> 0 then exit 0 ; 
+            let inchan = Unix.in_channel_of_descr s in
+            let outchan = Unix.out_channel_of_descr s in
+            server_fun caller inchan outchan ;
+            close_in inchan ;
+            close_out outchan ;
+            exit 0;
+        | id -> Unix.close s; ignore(Unix.waitpid [] id)
+    done
+
+let main_server serv_fun =
+  if Array.length Sys.argv < 3 then
+    Printf.eprintf "ERROR: Not enough args given - specify ip/host as $1 and port number as $2'\n"
+  else try
+    let my_address = Unix.inet_addr_of_string Sys.argv.(1) in
+    let port = int_of_string Sys.argv.(2) in
+    establish_server serv_fun (Unix.ADDR_INET(my_address, port))
+  with
+  | Failure e ->
+    match e with
+    | "inet_addr_of_string" -> Printf.printf "ERROR: Invalid IP address\n"
+    | "int_of_string" -> Printf.printf "ERROR: Invalid TCP port\n"
+    | _ -> Printf.printf "ERROR: %s\n" e
+
+let echosock_server caller ic oc =
+  try
+    while true do    
+      let s = input_line ic in 
+      let caller_s =
+        match caller with
+        | Unix.ADDR_INET (a,b) -> (Unix.string_of_inet_addr a)^":"^(string_of_int b) 
+        | _ -> ""
+      in
+      match caller_s with
+      | "" -> output_string oc (s^"\n") ;
+      | _ -> output_string oc ("from "^caller_s^" - "^s^"\n") ;
+      flush oc ;
+    done
+  with _ ->
+    Printf.printf "End of text\n" ;
+    flush (Unix.out_channel_of_descr Unix.stdout) ;
+    exit 0
+
+let go_uppercase_service () = 
+  Unix.handle_unix_error main_server echosock_server
+;;
+
+go_uppercase_service ()
