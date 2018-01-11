be14065f9c4fe014bd9849c0a8bd12ff8f948867
Bramford Horton <bram.horton@gmail.com>
2018-01-12 00:08:43 +1300

    Minor refactor of lwt list iter example

--- lwt/lwt_iter_list_tuples_example.ml
+++ lwt/lwt_iter_list_tuples_example.ml
@@ -10,7 +10,8 @@ let () =
 let () =
   let pt lwttpl =
     lwttpl >>= fun (a,b) ->
-    Lwt_io.printf "%s, %s\n%!" a b
+    Lwt_unix.sleep (4.0 -. b) >>= fun () ->
+    Lwt_io.printf "%s, %f\n%!" a b
   in
-  let my_tuple_list_lwt = [Lwt.return ("a","b"); Lwt.return ("c","d")] in
+  let my_tuple_list_lwt = [Lwt.return ("a",1.); Lwt.return ("b",2.)] in
   Lwt_main.run (Lwt_list.iter_p pt my_tuple_list_lwt)
