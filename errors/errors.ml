799ed41cb19f6bfcf3035536d123397c92614b25
Bramford Horton <bram.horton@gmail.com>
2018-07-07 23:03:09 +1200

    Fix errors example

--- errors/errors.ml
+++ errors/errors.ml
@@ -3,9 +3,7 @@ let herp = [ Ok 3; Error "some error"; Ok 4; Error "some other error" ];;
 let derp x =
   match x with
   | Ok o -> "OK: "^string_of_int o
-  | Error e ->
-    match e with
-    | string -> "ERROR: "^e
+  | Error e -> "ERROR: "^e
 ;;
 
 List.iter (fun i -> print_endline (derp i)) herp
