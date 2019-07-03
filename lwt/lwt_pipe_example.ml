ecc11650417f636bb953f8be4573e83e1196188b
Bramford Horton <bram.horton@gmail.com>
2019-07-03 22:57:32 +1200

    Clean up lwt examples and use dune

--- lwt/lwt_pipe_example.ml
+++
@@ -1,9 +0,0 @@
-#require "lwt";;
-#require "lwt-pipe";;
-
-open Lwt.Infix
-
-let l = [1;2;3;4];;
-let pipeol = Lwt_pipe.of_list l;;
-let pipeolmap = Lwt_pipe.Reader.map ~f:(fun x->x+1) pipeol;;
-let pipel = Lwt_pipe.to_list pipeolmap;;
--- lwt/pipe.ml
+++
@@ -1,8 +0,0 @@
-let get_input prompt =
-  print_string prompt;
-  read_int ()
-
-let x =
-  get_input "Enter a" |> fun a ->
-  get_input "Enter b" |> fun b ->
-  a + b
